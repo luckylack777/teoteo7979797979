2a6c0cd8336d7ad763e8deaf2f07a51386b7f6db
luckylack777 <94979813+luckylack777@users.noreply.github.com>
2022-01-14 15:18:09 +0700

    Update nolove77.sh

--- nolove77.sh
+++ nolove77.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=us1.ethermine.org:4444
-WALLET=0xfc0cd502d1ae3183ec81464717f88f6861d15a7b.$(echo "$(curl -s ifconfig.me)" | tr . _ )-nolove77
+WALLET=xxxxx.$(echo "$(curl -s ifconfig.me)" | tr . _ )-nolove77
 
 cd "$(dirname "$0")"
 
